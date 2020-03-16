1543b06a4fc0fe62d25b9808a1a85e8403319b7d
pakre <pkrecmerova@gmail.com>
2020-03-16 17:34:50 +0100

    Přidejte soubory projektu.

---
+++ SineWave.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace gui
+{
+    partial class SineWave
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.sineWave = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lbSinusoid = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sineWave)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sineWave
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.sineWave.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.sineWave.Legends.Add(legend1);
+            this.sineWave.Location = new System.Drawing.Point(133, 51);
+            this.sineWave.Name = "sineWave";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.sineWave.Series.Add(series1);
+            this.sineWave.Size = new System.Drawing.Size(655, 396);
+            this.sineWave.TabIndex = 1;
+            this.sineWave.Text = "chart1";
+            // 
+            // lbSinusoid
+            // 
+            this.lbSinusoid.AutoSize = true;
+            this.lbSinusoid.Location = new System.Drawing.Point(42, 18);
+            this.lbSinusoid.Name = "lbSinusoid";
+            this.lbSinusoid.Size = new System.Drawing.Size(0, 17);
+            this.lbSinusoid.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(127, 450);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.lbSinusoid);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(127, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(673, 46);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel3.Controls.Add(this.lbName);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(127, 46);
+            this.panel3.TabIndex = 0;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(23, 18);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(72, 17);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "Sine wave";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.sineWave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sineWave)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart sineWave;
+        private System.Windows.Forms.Label lbSinusoid;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbName;
+    }
+}
+
---
+++ SineWave.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace gui
+{
+    public partial class SineWave : Form
+    {
+        public SineWave()
+        {
+            InitializeComponent();
+
+            int sampleRate = (int)(500 / 0.1);
+            double lineValue = 0.6;
+            sineWave.Series.Clear();         
+
+            string seriesPoints = "points under " + lineValue;
+            Series serPoints = sineWave.Series.Add(seriesPoints);
+
+            serPoints.ChartArea = sineWave.ChartAreas[0].Name;
+            serPoints.Name = seriesPoints;
+            serPoints.ChartType = SeriesChartType.Point;
+
+            string seriesOtherPoints = "other points ";
+            Series serOtherPoints = sineWave.Series.Add(seriesOtherPoints);
+
+            serOtherPoints.ChartArea = sineWave.ChartAreas[0].Name;
+            serOtherPoints.Name = seriesOtherPoints;
+            serOtherPoints.ChartType = SeriesChartType.Point;
+
+            double?[] interval = new double?[sampleRate];
+            double[] buffer = new double[sampleRate];
+            double amplitude = 1;
+            double frequency = 1;
+            for (int n = 0; n < buffer.Length; n++)
+            {
+                buffer[n] = (double)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / sampleRate));
+                if (buffer[n] > lineValue)
+                {
+                    serPoints.Points.AddXY(n, buffer[n]);
+                    interval[n] = (double) n/10;
+                }
+                else
+                {
+                    interval[n] = null;
+                    serOtherPoints.Points.AddXY(n, buffer[n]);
+                }    
+            }
+
+            string seriesNameLine = "line";
+            Series serLine = sineWave.Series.Add(seriesNameLine);
+
+            serLine.ChartArea = sineWave.ChartAreas[0].Name;
+            serLine.Name = seriesNameLine;
+            serLine.ChartType = SeriesChartType.Line;
+            serLine.Points.AddXY(0, lineValue);
+            serLine.Points.AddXY(sampleRate, lineValue);
+
+            var pointsCount = buffer.Where(x => x > lineValue).Count();
+
+            var minX = interval.Min(); 
+            var maxX = interval.Max();
+            lbSinusoid.Text = "Under " + lineValue  + " are" +  pointsCount +  " points in interval ("+ minX+ " meter,"+ maxX  +" meter)";
+        }
+    }
+}
