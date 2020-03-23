477cdd66c55af918265a646c471949d9b143fa68
pakre <pkrecmerova@gmail.com>
2020-03-23 23:13:55 +0100

    Interface

--- SineWaveGui.cs
+++ SineWaveGui.cs
@@ -15,28 +15,69 @@ namespace gui
     {
         public SineWaveGui()
         {
-            InitializeComponent();
+            // generate
+            Generate generateSin = new Generate();
+            generateSin.generate();
 
-            int sampleRate = (int)(500 / 0.1);
-            double lineValue = 0.6;
+            // analysis
+            var pointsCount = generateSin.buffer.Where(x => x > generateSin.lineValue).Count();
+            double? minX = generateSin.interval.Min(); 
+            double? maxX = generateSin.interval.Max();
+            string outputLine = "Under " + generateSin.lineValue  + " are" +  pointsCount +  " points in interval("+ minX+ " meter, "+ maxX  +" meter)";
+
+            // gui
+            InitializeComponent();
             sineWave.Series.Clear();         
 
-            string seriesPoints = "points under " + lineValue;
+            string seriesPoints = "points under " + generateSin.lineValue;
             Series serPoints = sineWave.Series.Add(seriesPoints);
-
             serPoints.ChartArea = sineWave.ChartAreas[0].Name;
             serPoints.Name = seriesPoints;
             serPoints.ChartType = SeriesChartType.Point;
+            serPoints.Points.Add(generateSin.buffer);
+            for (int n = Convert.ToInt32(minX * 10); n < Convert.ToInt32(maxX*10); n++)
+                serPoints.Points.AddXY(n, generateSin.buffer[n]);
 
             string seriesOtherPoints = "other points ";
             Series serOtherPoints = sineWave.Series.Add(seriesOtherPoints);
-
             serOtherPoints.ChartArea = sineWave.ChartAreas[0].Name;
             serOtherPoints.Name = seriesOtherPoints;
             serOtherPoints.ChartType = SeriesChartType.Point;
+            for (int n = 0; n < Convert.ToInt32(minX * 10); n++)
+                serOtherPoints.Points.AddXY(n, generateSin.buffer[n]);
+            for (int n = Convert.ToInt32(maxX * 10); n< generateSin.buffer.Length; n++)
+                serOtherPoints.Points.AddXY(n, generateSin.buffer[n]);
 
-            double?[] interval = new double?[sampleRate];
-            double[] buffer = new double[sampleRate];
+            string seriesNameLine = "line";
+            Series serLine = sineWave.Series.Add(seriesNameLine);
+            serLine.ChartArea = sineWave.ChartAreas[0].Name;
+            serLine.Name = seriesNameLine;
+            serLine.ChartType = SeriesChartType.Line;
+            serLine.Points.AddXY(0, generateSin.lineValue);
+            serLine.Points.AddXY(generateSin.sampleRate, generateSin.lineValue);
+
+            lbSinusoid.Text = outputLine;
+        }
+    }
+
+
+    interface IGenerate
+    {
+        void generate();
+    }
+
+    class Generate : IGenerate
+    {
+        public double[] buffer;
+        public double lineValue = 0.6;
+        public int sampleRate;
+        public double?[] interval;
+
+        public void generate()
+        {
+            sampleRate = (int)(500 / 0.1);
+            interval = new double?[sampleRate];
+            buffer = new double[sampleRate];
             double amplitude = 1;
             double frequency = 1;
             for (int n = 0; n < buffer.Length; n++)
@@ -44,30 +85,13 @@ namespace gui
                 buffer[n] = (double)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / sampleRate));
                 if (buffer[n] > lineValue)
                 {
-                    serPoints.Points.AddXY(n, buffer[n]);
-                    interval[n] = (double) n/10;
+                    interval[n] = (double)n / 10;
                 }
                 else
                 {
                     interval[n] = null;
-                    serOtherPoints.Points.AddXY(n, buffer[n]);
-                }    
+                }
             }
-
-            string seriesNameLine = "line";
-            Series serLine = sineWave.Series.Add(seriesNameLine);
-
-            serLine.ChartArea = sineWave.ChartAreas[0].Name;
-            serLine.Name = seriesNameLine;
-            serLine.ChartType = SeriesChartType.Line;
-            serLine.Points.AddXY(0, lineValue);
-            serLine.Points.AddXY(sampleRate, lineValue);
-
-            var pointsCount = buffer.Where(x => x > lineValue).Count();
-
-            var minX = interval.Min(); 
-            var maxX = interval.Max();
-            lbSinusoid.Text = "Under " + lineValue  + " are" +  pointsCount +  " points in interval ("+ minX+ " meter,"+ maxX  +" meter)";
         }
     }
 }
